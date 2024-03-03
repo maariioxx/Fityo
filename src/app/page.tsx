70a5abe6952fc332db91ad10f9654cd2cd0ae2ad
mario <mariorb2017@gmail.com>
2024-03-03 22:39:56 +0100

    Check session and user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,35 @@
 import Link from 'next/link';
 import { Button } from 'flowbite-react';
-import sql from '@/database/db';
 import { cairo } from './fonts';
 import { redirect } from 'next/navigation';
-import Image from 'next/image';
-import IconsCarousel from '@/components/IconsCarousel';
+import IconsCarousel from '@/app/components/IconsCarousel';
+import { auth } from '@/auth/auth';
 
 export default async function Home() {
+  const session = await auth();
+  if (typeof session?.user !== 'undefined') redirect('/home');
   return (
-    <main className="pt-40 flex flex-col items-center grow justify-center gap-28">
+    <main className=" max-w-screen md:min-w-screen flex flex-col items-center justify-center gap-28">
       <IconsCarousel />
       <div className="flex flex-col gap-5 items-center">
         <h1
-          className={`${cairo.className} text-9xl font-semibold hover:text-green-700 transition-colors`}
+          className={`${cairo.className} text-8xl md:text-9xl xl:text-[10rem] font-semibold hover:text-green-700 transition-colors`}
         >
           <span className="text-green-700">Fit</span>yo
         </h1>
-        <p className="text-xl font-medium">Your fitness app</p>
+        <p className="text-md md:text-xl lg:text-2xl xl:text-xl font-medium">
+          Your fitness app
+        </p>
       </div>
-      <div className="flex gap-12">
-        <Link href="/signup">
-          <Button color="success">Sign up</Button>
-        </Link>
-        <Link href="/login">
-          <Button color="success">Log in</Button>
-        </Link>
-      </div>
+      <Link href="/signup">
+        <Button
+          size="xl"
+          className="w-28 h-12 md:w-32 md:h-16 lg:w-32 lg:h-16"
+          color="success"
+        >
+          <span className="text-sm md:text-base lg:text-lg">Join us!</span>
+        </Button>
+      </Link>
       <IconsCarousel />
     </main>
   );
