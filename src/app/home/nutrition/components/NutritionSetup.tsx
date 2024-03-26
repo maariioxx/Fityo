5fab83639bf4ca13cb1822604f1960efc460ef59
mario <mariorb2017@gmail.com>
2024-03-26 09:20:12 +0100

    Create nutrition setup page

---
+++ src/app/home/nutrition/components/NutritionSetup.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ToggleSwitch, Tooltip } from 'flowbite-react';
+import { useState } from 'react';
+import DailyForm from './DailyForm';
+import TotalForm from './TotalForm';
+
+export default function NutritionSetup() {
+  const [dailyNutrition, setDailyNutrition] = useState(false);
+  return (
+    <div className="py-20 flex flex-col items-center gap-10">
+      <div>
+        <Tooltip content="Choose between selecting your calories and macros based on a total or changing them depending on the day of the week">
+          <ToggleSwitch
+            checked={dailyNutrition}
+            onChange={setDailyNutrition}
+            label="Total / Daily"
+            color="success"
+          />
+        </Tooltip>
+      </div>
+      {dailyNutrition ? <DailyForm /> : <TotalForm />}
+    </div>
+  );
+}
