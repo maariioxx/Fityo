d62dbcdaeea76947c39cbcd702b94ef48155087d
mario <mariorb2017@gmail.com>
2024-03-26 09:18:10 +0100

    Create nutrition summary section

---
+++ src/app/home/components/nutrition/NutritionSummary.tsx
@@ -0,0 +1,32 @@
+import CaloriesProgressBar from './summary/CaloriesProgressBar';
+import { Suspense } from 'react';
+import {
+  CaloriesLoadingSkeleton,
+  CarbosLoadingSkeleton,
+  FatsLoadingSkeleton,
+  ProteinLoadingSkeleton,
+} from './summary/Skeletons';
+import CarbosProgressBar from './summary/CarbosProgressBar';
+import FatsProgressBar from './summary/FatsProgressBar';
+import ProteinProgressBar from './summary/ProteinProgressBar';
+
+export default function NutritionSummary({ date }: { date: string }) {
+  return (
+    <div className="grid lg:grid-cols-2 lg:gap-10 justify-center">
+      <Suspense fallback={<CaloriesLoadingSkeleton />}>
+        <CaloriesProgressBar date={date} />
+      </Suspense>
+      <div className="flex flex-col gap-7 -mt-2 lg:pt-3">
+        <Suspense fallback={<CarbosLoadingSkeleton />}>
+          <CarbosProgressBar date={date} />
+        </Suspense>
+        <Suspense fallback={<FatsLoadingSkeleton />}>
+          <FatsProgressBar date={date} />
+        </Suspense>
+        <Suspense fallback={<ProteinLoadingSkeleton />}>
+          <ProteinProgressBar date={date} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
