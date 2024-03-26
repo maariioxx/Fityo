342b4237ef95d85a9e99641433d47bbed316ff48
mario <mariorb2017@gmail.com>
2024-03-26 09:16:20 +0100

    Create error page

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+}
