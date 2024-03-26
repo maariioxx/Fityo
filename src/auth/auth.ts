bdeebf248b094f2587ce51ce9035b35bc3c1e1cd
mario <mariorb2017@gmail.com>
2024-03-26 09:25:17 +0100

    Add email provider

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -5,13 +5,31 @@ import {
 } from 'next';
 import { NextAuthOptions, getServerSession } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
 import { SupabaseAdapter } from '@next-auth/supabase-adapter';
 
-export const config = {
+export const config: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PW,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      maxAge: 15 * 60,
     }),
   ],
   secret: process.env.SECRET,
@@ -19,7 +37,11 @@ export const config = {
     url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
   }),
-} satisfies NextAuthOptions;
+  pages: {
+    signIn: '/signup',
+    verifyRequest: '/signup/verify/',
+  },
+};
 
 export function auth(
   ...args:
