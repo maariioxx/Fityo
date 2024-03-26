c073726a945b486b78568f099c6cb2c989937c8f
mario <mariorb2017@gmail.com>
2024-03-26 11:42:27 +0100

    Change form to Hook Form

--- src/app/home/nutrition/components/foods/CreateFood.tsx
+++ src/app/home/nutrition/components/foods/CreateFood.tsx
@@ -8,14 +8,32 @@ import {
   ModalBody,
   ModalFooter,
 } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import { createCustomFood } from '@/lib/actions';
+import { TCreateFoodSchema, createFoodSchema } from '@/types/forms/createfood';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function CreateFood() {
+  const { register, handleSubmit } = useForm<TCreateFoodSchema>({
+    resolver: zodResolver(createFoodSchema),
+  });
   const [openModal, setOpenModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleCreateFood = async (data: TCreateFoodSchema) => {
+    const res = await createCustomFood(data);
+    if (res) {
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 5000);
+    }
+  };
+
   return (
-    <div className="pt-0.5">
+    <div className="pt-0.5 relative">
       <Tooltip content="Create custom food">
         <Button color="success" size="sm" onClick={() => setOpenModal(true)}>
           <MdAdd className="text-2xl" />
@@ -23,13 +41,13 @@ export default function CreateFood() {
       </Tooltip>
       <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
         <ModalHeader>Create a custom food</ModalHeader>
-        <form action={createCustomFood}>
+        <form onSubmit={handleSubmit(handleCreateFood)}>
           <ModalBody className="grid grid-cols-2 gap-4">
             <label className="flex flex-col">
               Name:
               <input
+                {...register('name')}
                 type="text"
-                name="name"
                 className="form-input max-w-64"
                 required
               />
@@ -37,6 +55,7 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Quantity:
               <input
+                {...register('quantity')}
                 type="number"
                 name="quantity"
                 className="form-input max-w-64 number-input"
@@ -46,8 +65,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Calories:
               <input
+                {...register('calories')}
                 type="number"
-                name="calories"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -55,8 +74,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Carbohydrates:
               <input
+                {...register('carbohydrates')}
                 type="number"
-                name="carbohydrates"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -64,8 +83,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Sugar:
               <input
+                {...register('sugar')}
                 type="number"
-                name="sugar"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -73,8 +92,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Fats:
               <input
+                {...register('fats')}
                 type="number"
-                name="fats"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -82,8 +101,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Saturated fats:
               <input
+                {...register('saturated_fats')}
                 type="number"
-                name="saturated_fats"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -91,8 +110,8 @@ export default function CreateFood() {
             <label className="flex flex-col">
               Protein:
               <input
+                {...register('protein')}
                 type="number"
-                name="protein"
                 className="form-input max-w-64 number-input"
                 required
               />
@@ -102,13 +121,20 @@ export default function CreateFood() {
             <Button
               color="success"
               type="submit"
-              onClick={() => setOpenModal(false)}
+              onClick={() => setTimeout(() => setOpenModal(false), 1000)}
             >
               <MdAdd className="text-2xl" />
             </Button>
           </ModalFooter>
         </form>
       </Modal>
+      <div
+        className={`fixed top-[90vh] left-[45vw] w-48 pl-10 py-2 rounded-xl text-white bg-green-700 ${
+          showAlert ? 'scale-100 translate-y-0' : 'scale-0 -translate-y-8'
+        } transition-transform z-50`}
+      >
+        <p>Food created!</p>
+      </div>
     </div>
   );
 }
