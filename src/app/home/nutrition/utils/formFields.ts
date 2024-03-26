5e44722e920c9c2bac05c5903e55997d6c6260ef
mario <mariorb2017@gmail.com>
2024-03-26 09:23:06 +0100

    Create starting nutrition form fields

---
+++ src/app/home/nutrition/utils/formFields.ts
@@ -0,0 +1,47 @@
+export const FORM_FIELDS = {
+  monday_calories: '',
+  tuesday_calories: '',
+  wednesday_calories: '',
+  thursday_calories: '',
+  friday_calories: '',
+  saturday_calories: '',
+  sunday_calories: '',
+  monday_carbohidrates: '',
+  tuesday_carbohidrates: '',
+  wednesday_carbohidrates: '',
+  thursday_carbohidrates: '',
+  friday_carbohidrates: '',
+  saturday_carbohidrates: '',
+  sunday_carbohidrates: '',
+  monday_fats: '',
+  tuesday_fats: '',
+  wednesday_fats: '',
+  thursday_fats: '',
+  friday_fats: '',
+  saturday_fats: '',
+  sunday_fats: '',
+  monday_protein: '',
+  tuesday_protein: '',
+  wednesday_protein: '',
+  thursday_protein: '',
+  friday_protein: '',
+  saturday_protein: '',
+  sunday_protein: '',
+};
+
+export const FORM_TOTAL = {
+  totalCalories: '',
+  totalCarbohidrates: '',
+  totalFats: '',
+  totalProtein: '',
+};
+
+export const DAYS_FROM_TOTAL = {
+  mondayFromTotal: false,
+  tuesdayFromTotal: false,
+  wednesdayFromTotal: false,
+  thursdayFromTotal: false,
+  fridayFromTotal: false,
+  saturdayFromTotal: false,
+  sundayFromTotal: false,
+};
