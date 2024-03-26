6fce1a0675f3c180250657a2787abb64f2c74bb3
mario <mariorb2017@gmail.com>
2024-03-26 09:25:05 +0100

    Create verify email page

---
+++ src/app/signup/verify/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="text-center space-y-4">
+      <h1 className="text-2xl">Check your email!</h1>
+      <p>Email will expire in 15 minutes since it&apos;s been sent</p>
+    </div>
+  );
+}
