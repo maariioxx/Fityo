8afd5a032559f68b07dec27ad8270203d06dffc4
mario <mariorb2017@gmail.com>
2024-03-28 12:49:36 +0100

    Fix sections' borders

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
   const todayMeasures = await getMeasuresByDate();
   return (
     <div className="bg-slate-50 dark:bg-zinc-950 w-screen min-h-[91.4vh] mt-20 rounded-t-[100px] grid lg:grid-cols-2 lg:grid-rows-[75%,25%]">
-      <div className="row-start-1 row-end-3 border-2 rounded-t-[100px] lg:rounded-tr-[0px] border-slate-400 grid grid-rows-[20%,80%] justify-center">
+      <div className="row-start-1 row-end-3 border-2 rounded-t-[100px] lg:rounded-tr-[0px] border-slate-400 dark:border-zinc-800 grid grid-rows-[20%,80%] justify-center">
         <Link href="/home/nutrition">
           <h1 className="text-5xl pt-8 text-center">Nutrition</h1>
         </Link>
@@ -37,7 +37,7 @@ export default async function Page() {
           <HomeSetupButton link="/home/nutrition" />
         )}
       </div>
-      <div className="border-2 lg:rounded-tr-[100px] border-slate-400 grid grid-rows-[20%,80%] justify-center items-center justify-items-center">
+      <div className="border-2 lg:rounded-tr-[100px] border-slate-400 dark:border-zinc-800 grid grid-rows-[20%,80%] justify-center items-center justify-items-center">
         <Link href="/home/measures">
           <h1 className="text-5xl pt-4">Measures</h1>
         </Link>
