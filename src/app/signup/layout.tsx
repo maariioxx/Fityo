b4ba946cee074e37b5fa9c10da32a70dad4fa67e
mario <mariorb2017@gmail.com>
2024-04-07 13:40:34 +0200

    Change logos

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,15 +1,18 @@
 import { cairo } from '../fonts';
+import Image from 'next/image';
 
 export default function SignupLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-16">
-      <h1
-        className={`${cairo.className} text-6xl md:text-7xl font-bold text-center`}
-      >
-        Welcome to Fityo
-      </h1>
+      <Image
+        src="/assets/logo-128.svg"
+        width={128}
+        height={128}
+        alt="Fityo logo"
+        className="drop-shadow-xl"
+      />
       {children}
     </div>
   );
